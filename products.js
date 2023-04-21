954f4b49c5d7d6dae8c12ed6cebc3d74eb8038e4
Przemo <przemomomo@gmail.com>
2023-04-21 11:19:25 +0200

    changes

---
+++ products.js
@@ -0,0 +1,84 @@
+const products = [{
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    image: "https://i.dummyjson.com/data/products/1/1.jpg",
+  },
+  {
+    id: 2,
+    title: "iPhone 11",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    image: "https://i.dummyjson.com/data/products/1/1.jpg",
+  },
+  {
+    id: 3,
+    title: "Samsung Universe 9",
+    description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+    price: 1249,
+    stock: 36,
+    brand: "Samsung",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/3/1.jpg"
+    ]
+
+  },
+  {
+    id: 4,
+    title: "Samsung U20",
+    description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+    price: 1249,
+    stock: 36,
+    brand: "Samsung",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/3/1.jpg"
+    ]
+
+  },
+  {
+    id: 5,
+    title: "OPPOF19",
+    description: "OPPO F19 is officially announced on April 2021.",
+    price: 280,
+    stock: 123,
+    brand: "OPPO",
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+    images: [
+      "https://i.dummyjson.com/data/products/4/1.jpg",
+      "https://i.dummyjson.com/data/products/4/2.jpg",
+      "https://i.dummyjson.com/data/products/4/3.jpg",
+      "https://i.dummyjson.com/data/products/4/4.jpg",
+      "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+    ]
+  },
+  {
+  id: 6,
+  title: "MacBook Pro",
+  description: "MacBook Pro 2021 with mini-LED display may launch between September, November",
+  price: 1749,
+  stock: 83,
+  brand: "Apple",
+  category: "laptops",
+  thumbnail: "https://i.dummyjson.com/data/products/6/thumbnail.png",
+  "images": [
+    "https://i.dummyjson.com/data/products/6/1.png",
+    "https://i.dummyjson.com/data/products/6/2.jpg",
+    "https://i.dummyjson.com/data/products/6/3.png",
+    "https://i.dummyjson.com/data/products/6/4.jpg"
+  ]
+},
+]
